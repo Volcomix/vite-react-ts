7066d60dd53a1309fbab87441ac81edecfea4e68
Volcomix <Volcomix@users.noreply.github.com>
2022-09-09 20:25:26 +0200

    Setup stylelint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
-import reactLogo from './assets/react.svg'
 import './App.css'
+import reactLogo from './assets/react.svg'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="readTheDocs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
