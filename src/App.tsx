40d85f34d87d88b81869a460a08c88ed8083e415
Volcomix <Volcomix@users.noreply.github.com>
2022-09-09 20:11:02 +0200

    Setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 import reactLogo from './assets/react.svg'
 import './App.css'
 
@@ -8,10 +9,10 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
