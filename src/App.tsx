426ae486215169970a78af1c5a4b236c3494c9d0
Volcomix <Volcomix@users.noreply.github.com>
2022-09-10 10:26:19 +0200

    Setup CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
+import cx from 'classnames'
 import { useState } from 'react'
 
-import './App.css'
 import reactLogo from './assets/react.svg'
+
+import styles from './App.module.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className={styles.logo} alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className={cx(styles.logo, styles.react)}
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className={styles.card}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,7 +31,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="readTheDocs">
+      <p className={styles.readTheDocs}>
         Click on the Vite and React logos to learn more
       </p>
     </div>
