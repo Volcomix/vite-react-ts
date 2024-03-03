c3995cb1a7a4af7702b545f78fdd61246c7273d5
Volcomix <Volcomix@users.noreply.github.com>
2024-03-03 15:36:34 +0100

    Add more type checking to eslint

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
